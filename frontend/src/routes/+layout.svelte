98d2257855fed0b4dfc261ba460209fb91bdc384
elodie.laps <elodie.ida.laps@gmail.com>
2026-04-05 21:34:37 +0200

    fix layout

--- frontend/src/routes/+layout.svelte
+++ frontend/src/routes/+layout.svelte
@@ -10,15 +10,13 @@
 	});
 </script>
 
-<div class="min-h-screen bg-main-gradient text-mist-400">
-	<div class="flex flex-col sm:flex-col-reverse">
-		<div>
-			<main class="p-6">
+<div class="bg-main-gradient text-mist-400">
+	<div class="flex min-h-screen flex-col sm:flex-col-reverse">
+		<div class="flex flex-1 flex-col">
+			<main class="flex-1 p-6">
 				{@render children()}
 			</main>
-			<footer class="absolute bottom-0 flex w-screen justify-center bg-mauve-800 p-4">
-				game - elodie - 2026
-			</footer>
+			<footer class="flex w-full justify-center bg-mauve-800 p-4">game - elodie - 2026</footer>
 		</div>
 		<div class="sticky bottom-0 sm:top-0">
 			<Header />
